716685b9dea731ad65981e57e9d1122e2e726ce2
Steven Guerrero <44274802+iStevenGuerrero@users.noreply.github.com>
2020-06-24 18:18:09 -0500

    Update Lunh Validator.py

--- Lunh Validator.py
+++ Lunh Validator.py
@@ -1,5 +1,5 @@
-Number = str(input("Insert the number you'd like to check: "))
+number = str(input("Insert the number you'd like to check: "))
 suma = 0
-for i in range(len(Number)//2):
-    suma += int(Number[2*i])*2//10 + int(Number[2*i])*2%10 + int(Number[2*i+1])//10 + int(Number[2*i+1])%10 
+for i in range(len(number)//2)
+    suma += int(number[2*i])*2//10 + int(number[2*i])*2%10 + int(number[2*i+1])//10 + int(number[2*i+1])%10 
 print('Valid Lunh.') if(suma%10 == 0) else print('Invalid Lunh.')
